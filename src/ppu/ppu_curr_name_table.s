35b008cd1a3617e1adc82f097f9f68a7877ef3b2
Andre Paris <andreparis@gmail.com>
2018-09-06 11:37:10 -0300

    default mirror to vertical

---
+++ src/ppu/ppu_curr_name_table.s
@@ -0,0 +1,17 @@
+    .importzp _ppu 
+    .export _ppu_curr_name_table 
+
+ctrl = _ppu+0 
+
+; u16 __fastcall__ ppu_curr_name_table();
+.proc _ppu_curr_name_table 
+	lda ctrl 
+	and #%00000011
+	asl A 
+	asl A 
+	clc 
+	adc #$20
+	tax 
+	lda #0
+	rts 
+.endproc
